d35f9a4f5b0ee8eb4ced41390676eb761e54f7dd
Clive Messer <clive.m.messer@gmail.com>
2013-03-28 21:03:24 +0000

    Yesting execAndWait

---
+++ CommunitySqueeze/WebContent/wait.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+<meta http-equiv="refresh" content="5;url=<s:url includeParams="all" />" />
+<title>Community Squeeze OS Web Configuration: Processing...</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="images/favicon.ico" rel="icon" type="image/x-icon" />
+<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+</head>
+ 
+<body>
+<center>
+<h1>Processing...</h1>
+<img src="images/cs-logo-541x288.png" alt="Community Squeeze Logo" 
+  width="541" height="288" />
+<h2>Please wait while we process your request...</h2>
+</center>
+</body>
+</html>
---
+++ CommunitySqueeze/src/org/communitysqueeze/web/TestAction.java
@@ -0,0 +1,40 @@
+/**
+ *  Community Squeeze Configuration Web-GUI
+ *
+ *  Copyright (C) 2013  Clive Messer <clive.m.messer@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.communitysqueeze.web;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Clive Messer <clive.m.messer@gmail.com>
+ *
+ */
+public class TestAction extends ActionSupport {
+
+	private static final long serialVersionUID = 4359921946670926858L;
+
+	/* (non-Javadoc)
+	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 */
+	public String execute() throws Exception {
+	
+		//Thread.sleep(2000);
+		return SUCCESS;
+	}
+}
